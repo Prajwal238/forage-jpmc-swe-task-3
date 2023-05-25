412db5bd3b22b9445a704c11420ec772bf615136
Prajwal238 <prajwal_katakam@srmap.edu.in>
2023-05-25 13:18:00 +0530

    task3 completed

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,28 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  ratio: number,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
   timestamp: Date,
 }
 
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
+    const priceABC = (serverResponds[0].top_ask.price + serverResponds[0].top_bid.price) / 2;
+    const priceDEF = (serverResponds[1].top_ask.price + serverResponds[1].top_bid.price) / 2;
+    const r = priceABC/priceDEF;
+    const upper_bound = 1 + 0.1;
+    const lower_bound = 1 - 0.1;
+    const trigger_alert = (r > upper_bound || r < lower_bound) ? r : undefined;
       return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
+        ratio: r,
+        upper_bound: upper_bound,
+        lower_bound: lower_bound,
+        trigger_alert: trigger_alert,
+        timestamp: serverResponds[0].timestamp > serverResponds[1].timestamp ? serverResponds[0].timestamp : serverResponds[1].timestamp
       };
-    })
-  }
+    }
 }
